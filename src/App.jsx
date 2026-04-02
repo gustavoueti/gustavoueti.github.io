5480faa3f6c2db8f7c785d7bbd41841eb5a42435
gustavoueti <gustavo.ueti@yduqs.com.br>
2026-04-02 01:20:47 -0300

    add lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
+import { lazy, Suspense } from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
-import Home from './pages/Home/Home.jsx'
-import Post from './pages/Post/Post.jsx'
-import About from './pages/About/About.jsx'
+
+const Home = lazy(() => import('./pages/Home/Home.jsx'))
+const Post = lazy(() => import('./pages/Post/Post.jsx'))
+const About = lazy(() => import('./pages/About/About.jsx'))
 
 export default function App() {
   return (
     <HashRouter>
       <Header />
       <main style={{ flex: 1, paddingBlock: 'var(--spacing-lg)' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/post/:slug" element={<Post />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post/:slug" element={<Post />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Suspense>
       </main>
       <Footer />
     </HashRouter>
