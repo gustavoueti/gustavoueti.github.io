d92bb9d8f399c160a807a178afedc7c1dbe42741
gustavoueti <gustavo.ueti@yduqs.com.br>
2026-04-02 01:16:02 -0300

    fix format date

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,6 +1,6 @@
 export function formatDate(dateStr) {
   const date = new Date(dateStr + 'T00:00:00')
-  return date.toLocaleDateString('pt-BR', {
+  return date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
